4694b72f66a701eca3e5aca002e3ccbfa75a522e
Alex7498-SV <00068817@uca.edu.sv>
2018-06-21 16:50:31 -0600

    agragacion de validacion de nombre de usuario (funcio)

--- src/General/Usuario.java
+++ src/General/Usuario.java
@@ -1,13 +1,15 @@
 
 package General;
 
+import java.util.Scanner;
 
 public class Usuario {
     
+    Scanner scn =  new Scanner(System.in);
     private String username, score;
+    boolean flag;
 
-    public Usuario() {
-    }
+    public Usuario(){} 
     
     public Usuario(String username, String score) {
         this.username = username;
@@ -33,6 +35,49 @@ public class Usuario {
     public void mostrar(){
         Usuario usu = new Usuario();
         System.out.println("Username: " + usu.getUsername() + " || Score: " + usu.getScore());
+    }
+    
+
+
+    public String getNombre() {
+        return this.username;
+    }
+    
+    public void pedirNombre(){
+        while(flag){
+            //nombre = scn.nextLine();
+            if(setNombre(scn.nextLine())){
+                flag = false;
+            }else{
+                System.out.println("Nombre invalido, intenta de nuevo");
+
+            }
+        }
+    }
+
+    public boolean setNombre(String nombre) {
+        if(Validar(nombre)){
+            this.username = nombre;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public boolean Validar(String username){
+        char usrname;
+        if (username.length()>2 && username.length()<12){
+            for (int i = 0; i<username.length(); i++){
+                usrname = username.charAt(i);
+                if(!(Character.isAlphabetic(usrname) || Character.isDigit(usrname))){
+                    return false;
+                }
+            }
+        } else {
+            return false;
+        }
+        
+        return true;
     }
     
 }
