a4c4838db321546c9e1a6f63e067cf353555b1f7
Alex7498-SV <00068817@uca.edu.sv>
2018-06-20 23:41:50 -0600

    Cracion del Director del patron builder

---
+++ src/Villanos/ClanEnemigos.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Villanos;
+
+/**
+ *
+ * @author Dell-Studio1558
+ */
+public class ClanEnemigos {
+    private VillanoBuilder vBuilder;
+ 
+    public void setVillanoBuilder(VillanoBuilder vb){
+        vBuilder = vb;
+    }
+    
+    public Enemigo getEnemigo(){
+        return vBuilder.getEnemigo();
+    }
+    
+    public void crearEnemigo(){
+        vBuilder.buildVida(0);
+        vBuilder.buildAtaque(0);
+        vBuilder.buildTipo(type);
+        vBuilder.buildTipoRecompensa(recom);
+    }
+}
