336a328b4bcd05f4bf65b04f681972cf74bec968
Alex7498-SV <00068817@uca.edu.sv>
2018-06-21 10:29:02 -0600

    modificacion del paquete villanos

--- src/Villanos/AbstractVillano.java
+++ src/Villanos/AbstractVillano.java
@@ -9,11 +9,7 @@ public class AbstractVillano {
         EnemigoNivel2 en2 = new EnemigoNivel2();
         EnemigoNivel3 en3 = new EnemigoNivel3();
         EnemigoNivel4 en4 = new EnemigoNivel4();
-        BossN1 b1 = new BossN1();
-        BossN2 b2 = new BossN2();
-        BossN3 b3 = new BossN3();
-        BossN4 b4 = new BossN4();
-        
+
         switch(selec){
             case 1:
                 cE.setVillanoBuilder(en1);
@@ -31,22 +27,7 @@ public class AbstractVillano {
                 cE.setVillanoBuilder(en4);
                 cE.crearEnemigo(incv, incat, objeto);
                 return cE.getEnemigo();
-            case 5:
-                cE.setVillanoBuilder(b1);
-                cE.crearEnemigo(incv, incat, objeto);
-                return cE.getEnemigo();
-            case 6:
-                cE.setVillanoBuilder(b2);
-                cE.crearEnemigo(incv, incat, objeto);
-                return cE.getEnemigo();
-            case 7:
-                cE.setVillanoBuilder(b3);
-                cE.crearEnemigo(incv, incat, objeto);
-                return cE.getEnemigo();
-            case 8:
-                cE.setVillanoBuilder(b4);
-                cE.crearEnemigo(incv, incat, objeto);
-                return cE.getEnemigo();
+            
        }
         return null;
     }
