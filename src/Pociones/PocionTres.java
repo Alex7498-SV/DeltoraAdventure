02fcd3cbdbcbab9eb50905f44abb701730345859
stevenadiazr <00184117@uca.edu.sv>
2018-06-22 13:25:05 -0600

    La pocion que resta pasa a ser la pocion misteriosa. Esta sirve mediante un numero aleatorio que al sacarle el modulo dice si va a sumar o va a restar la vida al personaje

--- src/Pociones/PocionTres.java
+++ src/Pociones/PocionTres.java
@@ -15,20 +15,36 @@ public class PocionTres implements Pocion{
     public int sumaVida = 50;
     public int restaVida = 30;
     public int sumaFullVida = 100;
+    public boolean suma;
+    
+    public boolean numeroAleatorio(){
+        int aleatorio = (int)(Math.random()* 100)+1;
+        if(aleatorio % 2 == 0){
+            suma = true;
+        }
+        else{
+            suma = false;
+        }
+        return suma;
+    }
 
     @Override
     public void sumarDanio() {
-        //System.out.println(sumaDanio);
+//        System.out.println(sumaDanio); 
     }
 
     @Override
     public void sumarVida() {
-        //System.out.println(sumaVida);
+        if(suma == true){
+            System.out.println(sumaVida);
+        } 
     }
 
     @Override
     public void restarVida() {
-        System.out.println(restaVida);
+        if (suma == false){
+            System.out.println(restaVida);
+        }
     }
 
     @Override
