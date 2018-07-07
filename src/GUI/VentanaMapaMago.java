838e72110946d4f78f2a96fc4756e8947cc9d12b
David Guardado <00184217@uca.edu.sv>
2018-07-06 21:19:47 -0600

    agregando hilos enemigos al mapa del mago xddd

--- src/GUI/VentanaMapaMago.java
+++ src/GUI/VentanaMapaMago.java
@@ -52,74 +52,56 @@ public class VentanaMapaMago extends JFrame {
         for (int i = 0; i <= 12; i++) {
             if (i == 0) {
                 labels[i] = new JLabel();
-                labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(200, 150, 40, 40);
+                labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/guerrero1.png")));
+                labels[i].setBounds(250, 100, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 1) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(200, 400, 40, 40);
+                labels[i].setBounds(200, 300, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 2) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(100, 540, 40, 40);
+                labels[i].setBounds(250, 550, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 3) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(300, 540, 40, 40);
+                labels[i].setBounds(650, 500, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 4) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(500, 540, 40, 40);
+                labels[i].setBounds(600, 700, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 5) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(860, 540, 40, 40);
+                labels[i].setBounds(860, 300, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 6) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(900, 380, 40, 40);
+                labels[i].setBounds(800, 50, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 7) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(825, 175, 40, 40);
+                labels[i].setBounds(500, 175, 40, 40);
                 container.add(labels[i]);
             }
             if (i == 8) {
                 labels[i] = new JLabel();
                 labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(750, 55, 40, 40);
-                container.add(labels[i]);
-            }
-            if (i == 9) {
-                labels[i] = new JLabel();
-                labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(625, 55, 40, 40);
-                container.add(labels[i]);
-            }
-            if (i == 10) {
-                labels[i] = new JLabel();
-                labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(500, 55, 40, 40);
-                container.add(labels[i]);
-            }
-            if (i == 11) {
-                labels[i] = new JLabel();
-                labels[i].setIcon(new ImageIcon(getClass().getResource("/personajeimagen/mago1.jpg")));
-                labels[i].setBounds(500, 270, 40, 40);
+                labels[i].setBounds(500, 300, 40, 40);
                 container.add(labels[i]);
             }
         }
@@ -133,9 +115,6 @@ public class VentanaMapaMago extends JFrame {
         HiloArribaAbajo enemigo6 = new HiloArribaAbajo("enemigo6", 400, labels[6], labels[6].getX(), labels[6].getY());
         HiloArribaAbajo enemigo7 = new HiloArribaAbajo("enemigo7", 200, labels[7], labels[7].getX(), labels[7].getY());
         HiloDerechaIzquierda enemigo8 = new HiloDerechaIzquierda("enemigo8", 775, labels[8], labels[8].getX(), labels[8].getY());
-        HiloDerechaIzquierda enemigo9 = new HiloDerechaIzquierda("enemigo9", 650, labels[9], labels[9].getX(), labels[9].getY());
-        HiloDerechaIzquierda enemigo10 = new HiloDerechaIzquierda("enemigo10", 525, labels[10], labels[10].getX(), labels[10].getY());
-        HiloArribaAbajo enemigo11 = new HiloArribaAbajo("enemigo11", 300, labels[11], labels[11].getX(), labels[11].getY());
         enemigo0.start();
         enemigo1.start();
         enemigo2.start();
@@ -145,9 +124,7 @@ public class VentanaMapaMago extends JFrame {
         enemigo6.start();
         enemigo7.start();
         enemigo8.start();
-        enemigo9.start();
-        enemigo10.start();
-        enemigo11.start();
+      
         personaje.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
