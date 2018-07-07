d2d717ee5f640a0f81c983c0ba2d07678eb32064
DGuardado19 <37202984+DGuardado19@users.noreply.github.com>
2018-07-06 23:52:47 -0600

    Corrigiendo en base a imagenes nuevas de los hilos

--- src/hilosdenemigos/HiloDerechaIzquierda.java
+++ src/hilosdenemigos/HiloDerechaIzquierda.java
@@ -5,6 +5,7 @@
  */
 package hilosdenemigos;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 /**
@@ -41,12 +42,75 @@ public class HiloDerechaIzquierda extends Thread {
              for(int i=x; i<= this.limite; i+=15){
                  this.enemigo.setLocation(i, y);
                  try {
-                     if (this.nombre.equals("boss")) {
-                         sleep(100);
-                     } else {
+                     if (this.nombre.equals("Mago8")) {
+                          this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
                          sleep(250);
-                     }
-
+                     } 
+                     else if (this.nombre.equals("Mago0")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("Mago1")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("Mago2")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("Mago3")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("Mago4")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("Mago5")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("Mago6")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("Mago7")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo0")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo1")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo2")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo3")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo4")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo5")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo6")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo7")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo8")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo9")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo10")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanD.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo11")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/BossNathanD.png")));
+                        sleep(250);
+                    } else {
+                        // this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusF.png")));
+                        sleep(250);
+                    }
+                   
                  } catch (InterruptedException e) {
                      e.printStackTrace();
                  }
@@ -54,10 +118,71 @@ public class HiloDerechaIzquierda extends Thread {
             for(int i=this.limite; i>= x; i-=15){
                 this.enemigo.setLocation(i, y);
                 try{
-                    if(this.nombre.equals("boss")){
-                        sleep(100);
-                    }
-                    else{
+                     if (this.nombre.equals("Mago8")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/BossVeneficusI.png")));
+                        sleep(250);}
+                        else if (this.nombre.equals("Mago0")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                     else if (this.nombre.equals("Mago1")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                      else if (this.nombre.equals("Mago2")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                       else if (this.nombre.equals("Mago3")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                        else if (this.nombre.equals("Mago4")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                         else if (this.nombre.equals("Mago5")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                          else if (this.nombre.equals("Mago6")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                           else if (this.nombre.equals("Mago7")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusI.png")));
+                        sleep(250);}
+                     else if (this.nombre.equals("enemigo0")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo1")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo2")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo3")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo4")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo5")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo6")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo7")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo8")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo9")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo10")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/enemigoNathanI.png")));
+                        sleep(250);
+                    } else if (this.nombre.equals("enemigo11")) {
+                        this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesEspartano/BossNathanI.png")));
+                        sleep(250);
+                    } else {
+                        //this.enemigo.setIcon(new ImageIcon(getClass().getResource("/imagenesMago/enemigoVeneficusT.png")));
                         sleep(250);
                     }
 
